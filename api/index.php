7d0b35c217d32892c4c4561aeb0aa16826b7d63b
pierreboissinot <pierre.boissinot@lephare.com>
2023-11-10 14:49:33 +0100

    feat(slack): interact with mention using Bot

--- api/index.php
+++ api/index.php
@@ -1,15 +1,108 @@
 <?php
 
+require_once 'functions/gitlab_functions.php';
+
 // Requires OPENAI_API_TOKEN OpenAI API secret key
+// Requires SLACK_BOT_USER_OAUTH_TOKEN
 
 // TODO: handle rate limit ? sleep ?
 
-require_once 'functions/gitlab_functions.php';
+$jsonData = file_get_contents('php://input');
+//unlink('php://input');
+
+//echo "jsonData" . $jsonData . "\n";
+
+/*
+echo json_encode([
+    'challenge' => json_decode($jsonData, true)['challenge'],
+]);
+die();
+*/
+$slackEvent = json_decode($jsonData, true);
+$userText = $slackEvent['event']['blocks'][0]['elements'][0]['elements'][1]['text'];
+$botReplyMessage = $userText;
+$channel = $slackEvent['event']['channel'];
+$eventTs = $slackEvent['event']['ts'];
+
 
 const gptModel = 'gpt-3.5-turbo-1106'; // supports parallels functions calling
 
+function callbackSlack(string $responseUrl, string $content): void
+{
+    $url = $responseUrl;
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 3,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_FAILONERROR => false,
+        CURLOPT_POSTFIELDS => $content,
+    ));
+
+    $response = curl_exec($curl);
+
+    if (curl_errno($curl)) {
+        throw new Exception(sprintf("Error during %s %s with body \n%s\nError : %s", 'POST', $url, $content, curl_error($curl)));
+    }
+
+    curl_close($curl);
+
+    if (!is_string($response)) {
+        die(sprintf('failure %s %s', 'POST', $url));
+    }
+}
+
+// See https://api.slack.com/messaging/sending
+function slackReplyInThread(string $channel, string $threadTs, string $message): void
+{
+    //echo "slackReplyInThread\n";
+    $curl = curl_init();
+
+    $url = 'https://slack.com/api/chat.postMessage';
+
+    $body = [
+        "channel" => $channel,
+        "thread_ts" => $threadTs,
+        "text" => $message,
+    ];
+
+    $payload = json_encode($body);
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 3,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_FAILONERROR => true,
+        CURLOPT_POSTFIELDS => $payload,
+        CURLOPT_HTTPHEADER => array(
+            sprintf("Authorization: Bearer %s", getenv('SLACK_BOT_USER_OAUTH_TOKEN')),
+            "Content-Type: application/json",
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    //var_dump($response);
+
+    if (curl_errno($curl)) {
+        throw new Exception(sprintf("Error during %s %s with body \n%s\nError : %s", 'POST', $url, $payload, curl_error($curl)));
+    }
+
+    curl_close($curl);
+
+    if (!is_string($response)) {
+        die(sprintf('failure %s %s', 'POST', $url));
+    }
+}
+
 function handleOpenAiRequest(string $path, string $method = 'GET', string $body = null): string
 {
+    //echo "openAiRequest with payload: \n";
+    //echo json_encode($body, JSON_PRETTY_PRINT);
     $url = sprintf("https://api.openai.com/v1%s", $path);
     $openAiApiToken = getenv('OPENAI_API_TOKEN');
     $curl = curl_init();
@@ -84,13 +177,10 @@ function buildPayload(array $messages): array {
 
 // Slack command timeout if the api does not respond in less than 3000ms
 // See https://api.slack.com/interactivity/slash-commands#responding_basic_receipt
-set_time_limit(0);
-ob_implicit_flush(1);
-ob_end_flush();
-echo "Un instant svp ...";
 
 // TODO: get user input
-$userInput = $_POST['text'];// example: "Quelles sont les MR à relire concernant la branche s47 ?";
+//$userInput = $_POST['text'];// example: "Quelles sont les MR à relire concernant la branche s47 ?";
+$userInput = $userText;
 
 $payload = json_encode(buildPayload(
     [
@@ -136,47 +226,31 @@ $secondPayload = json_encode(buildPayload(
 // var_dump($secondPayload);
 
 // Call the model with functions and the user’s input
-$secondResponse = handleOpenAiRequest("/chat/completions", "POST", $secondPayload);
+try {
+    $secondResponse = handleOpenAiRequest("/chat/completions", "POST", $secondPayload);
+} catch (Exception $e) {
+    die('erreur');
+}
 
 // var_dump($secondResponse);
 
 $data = json_decode($secondResponse, true);
 
+/*
 $slackResponse = [
     "response_type" => "in_channel",
     "text" => $data['choices']['0']['message']['content'],
 ];
+*/
+$slackResponse = $data['choices']['0']['message']['content'];
 
 
-function callbackSlack(string $responseUrl, string $content): void
-{
-    $url = $responseUrl;
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT => 10,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_FAILONERROR => false,
-        CURLOPT_POSTFIELDS => $content,
-    ));
-
-    $response = curl_exec($curl);
-
-    if (curl_errno($curl)) {
-        throw new Exception(sprintf("Error during %s %s with body \n%s\nError : %s", 'POST', $url, $content, curl_error($curl)));
-    }
-
-    curl_close($curl);
-
-    if (!is_string($response)) {
-        die(sprintf('failure %s %s', 'POST', $url));
-    }
-}
 
 
-callbackSlack($_POST['response_url'], json_encode($slackResponse));
+// callbackSlack($_POST['response_url'], json_encode($slackResponse));
+
+slackReplyInThread($channel, $eventTs, $slackResponse);
+
+//echo json_encode($slackResponse);
 
 // var_dump($secondResponse);
